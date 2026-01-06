12b0974dfe84df51910c71111a305a8181037439
harajukuxx <nutthapong.s@windowslive.com>
2026-01-06 22:27:08 +0700

    feat: add variable declarations and boolean type checking with typeof

---
+++ day01-1-variables.js
@@ -0,0 +1,25 @@
+let score = 10
+score = 10 +10
+console.log("🚀 ~ score:", score)
+
+let carname = "BMW"
+let firstname;  //undefined
+
+const fruits = ["Apple","Banana","KIWI"]
+console.log("🚀 ~ fruits:", fruits)
+
+//types
+console.log("typeof score:", typeof(score))
+console.log("typeof carname:", typeof(carname))
+console.log("typeof fruits:", typeof(fruits))
+console.log("isArray fruits:", Array.isArray(fruits))
+
+//booleans
+console.log(7 == 7)     //true
+console.log(7 == '7')   //true
+console.log(7 === '7')  //false
+console.log(7 != 7)     //false
+console.log(5 != 7)     //true
+
+
+
