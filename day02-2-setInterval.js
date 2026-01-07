9de37c0237251a2d1b42067fed0a247ed45b233a
harajukuxx <nutthapong.s@windowslive.com>
2026-01-07 21:49:07 +0700

    test clearInterval for stop setInterval

--- day02-2-setInterval.js
+++ day02-2-setInterval.js
@@ -1,3 +1,18 @@
+/*
 setInterval(() => {
   console.log("Tick...");
+}, 1000);
+*/
+
+let count = 0;
+
+// เก็บค่าไว้ในตัวแปร timer
+const timer = setInterval(() => {
+  count++;
+  console.log("วินาทีที่:", count);
+
+  if (count === 5) {
+    clearInterval(timer); // สั่งหยุดเมื่อถึง 5 วินาที
+    console.log("หยุดแล้ว!");
+  }
 }, 1000);
